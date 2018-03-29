532667b1ec4a8b5d313945ba4c0db1ef8c0dbb25
JBrown1342 <35350655+JBrown1342@users.noreply.github.com>
2018-03-29 12:28:36 -0700

    start

--- index.js
+++ index.js
@@ -21,35 +21,68 @@ app.use(function(req, res, next){
 
 pool = new Pool(config);
 
+const fs = require('fs');
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
+  let template = fs.readFileSync(__dirname + '/public/index.html');
+  let options = [];
+
+  pool.query('SELECT * FROM artists order by artist_name', (err, results) => {
+    console.log(results);
+
+    results.rows.forEach((row) => {
+      options.push('<option value="${row.id}">' + row.artist_name + '</option>');
+    });
+
+    options = options.join("\n");
+    template = template.toString().replace('@@artist_list_1@@', options);
+
+    res.set('Content-Type', 'text/html');
+    res.send(template);
+  });
 });
 
 app.get('/add_artist', function(req, res){
     res.sendFile(__dirname + '/public/add_artist.html');
 });
 
-/*
-app.post('/search', function(req, res){
-  console.log('Searching!');
-
-  ///// this is where I got stuck with the search option.
-
-  pool.query(query, (err, res) => {
-    if (err) {
-      console.log(err.stack)
-    } else {
-      console.log(res.rows)
-    }
-  });
+app.get('/add_venue', function(req, res){
+    res.sendFile(__dirname + '/public/add_venue.html');
 });
-*/
+
+app.get('/add_show', function(req, res){
+//////////SSSSSSSSTTTTTTUUUUUUCCCCCCKKKKKKKKKKK
+});
+
+app.post('/search', function(req, res){
+  pool.query('SELECT * FROM shows WHERE artist = 1', (err, res) => {
+    console.log(res);
+  });
+  res.send(res);
+});
 
 app.post('/artist_added', function(req, res){
   console.log('Posting!');
   const query = {
     text: 'INSERT INTO artists (artist_name, artist_website) VALUES($1, $2)',
     values: [req.body.artist_name, req.body.artist_website],
+  };
+
+  pool.query(query, (err, res) => {
+    if (err) {
+      console.log(err.stack)
+    } else {
+      console.log(res.rows[0])
+    }
+  })
+  res.send("Complete");
+});
+
+app.post('/venue_added', function(req, res){
+  console.log('Posting!');
+  const query = {
+    text: 'INSERT INTO venues (venue_name, venue_website, venue_city, venue_state) VALUES($1, $2, $3, $4)',
+    values: [req.body.venue_name, req.body.venue_website, req.body.venue_city, req.body.venue_state],
   }
   pool.query(query, (err, res) => {
     if (err) {
@@ -60,6 +93,7 @@ app.post('/artist_added', function(req, res){
   })
   res.send("Complete");
 });
+
 
 app.listen(3000, function(){
   console.log('Server started on port 3000')
--- show_database.sql
+++ show_database.sql
@@ -4,8 +4,44 @@ CREATE table artists (
   artist_website varchar (200)
 );
 
+CREATE table venues (
+  id SERIAL primary key,
+  venue_name varchar (100),
+  venue_website varchar (200),
+  venue_city varchar (50),
+  venue_state varchar (50)
+);
+
+CREATE table shows (
+  id SERIAL primary key,
+  artist integer references artists(id),
+  venue integer references venues(id),
+  show_date date,
+  show_time varchar (25),
+  price varchar (50)
+);
+
+
 INSERT INTO artists (artist_name, artist_website)
 VALUES ('Band 1', 'www.band1.com');
 
 INSERT INTO artists (artist_name, artist_website)
 VALUES ('Group 2', 'www.group2.com');
+
+
+INSERT INTO venues (venue_name, venue_website, venue_city, venue_state)
+VALUES ('Venue 1', 'www.Venue1.com', 'Boston', 'Massachusetts');
+
+INSERT INTO venues (venue_name, venue_website, venue_city, venue_state)
+VALUES ('2nd Venue', 'www.2ndVenue.com', 'New York', 'New York');
+
+
+INSERT INTO shows (show_date, show_time, price, artist, venue)
+SELECT to_date('2020-9-14', 'YYYY-MM-DD'), '5pm', '$10', artists.id, venues.id
+FROM artists, venues
+WHERE artist_name = 'Group 2' and venue_name = 'Venue 1';
+
+INSERT INTO shows (show_date, show_time, price, artist, venue)
+SELECT to_date('2019-11-28', 'YYYY-MM-DD'), '10pm', '$15', artists.id, venues.id
+FROM artists, venues
+WHERE artist_name = 'Band 1' and venue_name = '2nd Venue';
